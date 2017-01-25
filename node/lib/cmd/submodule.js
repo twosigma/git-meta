06cb17c18927d846a9bccf65e0bddcf654482326
Brock Peabody <brock.peabody@gmail.com>
2017-01-24 21:05:02 -0500

    Added `find-meta` command

--- node/lib/cmd/submodule.js
+++ node/lib/cmd/submodule.js
@@ -79,37 +79,52 @@ current SHA-1 for that submodule followed by its name.`,
 submodule is closed, followed by the current SHA-1 for \
 that submodule, followed by its name. `
     });
+
+    const findMetaParser = subParsers.addParser("find-meta", {
+        help: "find the meta-repo commit from a submodule commit",
+        description: `\
+given a meta-repo commit M and a sub-repo commit S, find the nearest \
+ancestor of M (or M itself) that references S (or a descendant of S)`,
+    });
+
+    findMetaParser.addArgument(["path"], {
+        type: "string",
+        help: "path to the submodule (or a path inside the submodule)",
+    });
+
+    findMetaParser.addArgument(["submodule committish"], {
+        help: "submodule commit to look for",
+        type: "string",
+        required: false,
+        defaultValue: "HEAD",
+        nargs: "?",
+    });
+
+    findMetaParser.addArgument(["-m", "--meta-committish"], {
+        help: "meta-repo commit from which to begin searching",
+        type: "string",
+        required: false,
+        defaultValue: "HEAD",
+    });
 };
 
-/**
- * Execute the `submodule` command according to the specified `args`.
- *
- * @async
- * @param {Object}  args
- * @param {Boolean} args.any
- * @param {String}  repository
- * @param {String}  [source]
- */
-exports.executeableSubcommand = co.wrap(function *(args) {
-    // TODO: right now, there is only one sub-command, "status".  Later we will
-    // have to split this out.
-
+const doStatusCommand = co.wrap(function *(paths, verbose) {
     const colors = require("colors");
     const path   = require("path");
 
-    const GitUtil       = require("../util/git_util");
-    const Status        = require("../util/status");
+    const GitUtil = require("../util/git_util");
+    const Status  = require("../util/status");
 
     const repo = yield GitUtil.getCurrentRepo();
     const workdir = repo.workdir();
     const cwd = process.cwd();
-    const paths = yield args.path.map(filename => {
+    paths = yield paths.map(filename => {
         return GitUtil.resolveRelativePath(workdir, cwd, filename);
     });
     const repoStatus = yield Status.getRepoStatus(repo, {
         paths: paths,
         showMetaChanges: false,
-        includeClosedSubmodules: args.verbose,
+        includeClosedSubmodules: verbose,
     });
     const subStats = repoStatus.submodules;
     const relCwd = path.relative(workdir, cwd);
@@ -126,7 +141,7 @@ exports.executeableSubcommand = co.wrap(function *(args) {
         });
     }
 
-    if (args.verbose) {
+    if (verbose) {
         console.log(`${colors.grey("All submodules: ")}`);
         doSummary(true);
     }
@@ -135,3 +150,131 @@ exports.executeableSubcommand = co.wrap(function *(args) {
         doSummary(false);
     }
 });
+
+const doFindCommand = co.wrap(function *(path, metaCommittish, subCommittish) {
+    const colors = require("colors");
+
+    const GitUtil          = require("../util/git_util");
+    const LogUtil          = require("../util/log_util");
+    const Open             = require("../util/open");
+    const SubmoduleFetcher = require("../util/submodule_fetcher");
+    const SubmoduleUtil    = require("../util/submodule_util");
+    const UserError        = require("../util/user_error");
+
+    // We need to resolve and validate the committishes and submodule path
+    // before calling `findMetaCommit` to do the real work.
+
+    const repo = yield GitUtil.getCurrentRepo();
+    const workdir = repo.workdir();
+
+    const subNames = yield SubmoduleUtil.getSubmoduleNames(repo);
+    const openSubNames = yield SubmoduleUtil.listOpenSubmodules(repo);
+
+    // Here, we find which submodule `path` refers too.  It migt be invalid by
+    // referring to no submodule, or by referring to more than one.
+
+    const relPath = yield GitUtil.resolveRelativePath(workdir,
+                                                      process.cwd(),
+                                                      path);
+    const resolvedPaths = yield SubmoduleUtil.resolvePaths(workdir,
+                                                           [relPath],
+                                                           subNames,
+                                                           openSubNames);
+    const paths = Object.keys(resolvedPaths);
+    if (0 === paths.length) {
+        throw new UserError(`No submodule found in ${colors.red(path)}.`);
+    }
+    if (1 !== paths.length) {
+        throw new UserError(`Multiple submodules: \
+${colors.yellow(paths.join(", "))} found in ${colors.red(path)}.`);
+    }
+
+    // Resolve `metaCommittish` first; if this resolve fails there is no reason
+    // to potentially open the sub-repo.
+
+    const metaAnnotated = yield GitUtil.resolveCommitish(repo, metaCommittish);
+    if (null === metaAnnotated) {
+        throw new UserError(`\
+Could not find ${colors.red(metaCommittish)} in the meta-repo.`);
+    }
+
+    // Create a `SubmoduleFetcher` to be used for opening the submodule, if
+    // closed, and for fetching commits during the search.
+
+    const head = yield repo.getHeadCommit();
+    const fetcher = new SubmoduleFetcher(repo, head);
+
+    const subName = paths[0];
+    let subRepo;
+
+    // Open the submodule if it's closed.
+
+    if (-1 === openSubNames.findIndex(x => x === subName)) {
+        console.log(`Opening ${colors.blue(subName)}.`);
+        const shas = yield SubmoduleUtil.getSubmoduleShasForCommit(repo,
+                                                                   [subName],
+                                                                   head);
+        const subSha = shas[subName];
+        subRepo = yield Open.openOnCommit(fetcher, subName, subSha);
+    }
+    else {
+        subRepo = yield SubmoduleUtil.getRepo(repo, subName);
+    }
+
+    const metaCommit = yield repo.getCommit(metaAnnotated.id());
+
+    // Now that we have an open submodule, we can attempt to resolve
+    // `subCommittish`.
+
+    const subAnnotated = yield GitUtil.resolveCommitish(subRepo,
+                                                        subCommittish);
+    if (null === subAnnotated) {
+        throw new UserError(`\
+Could not find ${colors.red(subCommittish)} in ${colors.blue(subName)}.\
+`);
+    }
+
+    // This command could take a while; let the user know exactly what we're
+    // doing.
+
+    console.log(`\
+Searching for ${colors.green(GitUtil.shortSha(subAnnotated.id().tostrS()))} \
+from submodule ${colors.blue(subName)}, starting at \
+${colors.green(GitUtil.shortSha(metaAnnotated.id().tostrS()))} in the \
+meta-repo.`);
+
+    const subCommit = yield subRepo.getCommit(subAnnotated.id());
+
+    // Finally, we do the search.
+
+    const result = yield LogUtil.findMetaCommit(repo,
+                                                metaCommit,
+                                                subCommit,
+                                                subName,
+                                                subRepo,
+                                                fetcher);
+    if (null === result) {
+        console.log("Not found.");
+    }
+    else {
+        console.log(`Found ${colors.green(result)}`);
+    }
+});
+
+/**
+ * Execute the `submodule` command according to the specified `args`.
+ *
+ * @async
+ * @param {Object}  args
+ * @param {Boolean} args.any
+ * @param {String}  repository
+ * @param {String}  [source]
+ */
+exports.executeableSubcommand = function (args) {
+    if ("status" === args.command) {
+        return doStatusCommand(args.path, args.verbose);
+    }
+    return doFindCommand(args.path,
+                         args.meta_committish,
+                         args["submodule committish"]);
+};
